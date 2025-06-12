f46509c6f5f4613b6d4f95d0db1ab6e907947610
NahueAdema <ademanahuel@gmail.com>
2025-06-12 19:35:27 -0300

    Configuracion Astro para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+  base: "/portfolio/",
+  output: "static",
 
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
